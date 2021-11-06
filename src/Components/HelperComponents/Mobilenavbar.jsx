42c3ba13213acb1b88363d828cff38a4565ca343
Aniket Kulkarni <anirajkul01@gmail.com>
2021-11-06 11:40:52 +0530

    hotjar added

--- src/Components/HelperComponents/Mobilenavbar.jsx
+++ src/Components/HelperComponents/Mobilenavbar.jsx
@@ -5,6 +5,7 @@ import { Link, useLocation } from 'react-router-dom';
 import { HiHome } from 'react-icons/hi';
 import { RiArrowDropDownLine, RiShoppingCartLine } from 'react-icons/ri';
 import { CgProfile } from 'react-icons/cg';
+import { motion } from 'framer-motion'
 
 
 const Mobilenavbar = () => {
@@ -36,7 +37,7 @@ const Mobilenavbar = () => {
         };
     }, []);
 
-    const togglenavbarbyclick = (e)=>{
+    const togglenavbarbyclick = (e) => {
         if (sidenavbarref.current && !sidenavbarref.current.contains(e.target)) {
             settogglenavbar(false);
         }
@@ -52,60 +53,64 @@ const Mobilenavbar = () => {
                     Shopeasy
                 </div>
             </div>
-            <div ref={sidenavbarref} className={" flex-no-wrap " + (togglenavbar ? "flex" : "hidden")}>
-                <div className="w-64 z-40 h-full absolute bg-gray-800 shadow flex-col justify-between ">
+            {
+                togglenavbar &&
 
-                    <div className="px-8">
-                        <div className="h-16 w-full flex items-center logo">
-                            <div className="logo  text-2xl  font-semibold w-full">
-                                Shopeasy
-                            </div>
-                        </div>
-                        <ul className="mt-12 text-white">
-                            <div className="p-4">
-                                <Link to="/">
-                                    <div className="flex items-center">
-                                        <HiHome />
-                                        <span className="pl-2">
-                                            Home
-                                        </span>
-                                    </div>
-                                </Link>
-                            </div>
-                            <div className="p-4">
+                <div  ref={sidenavbarref} className={" flex-no-wrap "}>
+                    <motion.div initial={{x:"-100vw"}} animate={{x:0}} transition={{type:"tween"}} className="w-64 z-40 h-full absolute bg-gray-800 shadow flex-col justify-between ">
 
-                                <div className="flex items-center" onClick={() => setdisplaydropdown(!displaydropdown)}>
-                                    <span className="pl-2">
-                                        Category
-
-                                    </span>
-                                    <RiArrowDropDownLine className="text-4xl pt-1" />
+                        <div className="px-8">
+                            <div className="h-16 w-full flex items-center logo">
+                                <div className="logo  text-2xl  font-semibold w-full">
+                                    Shopeasy
                                 </div>
-                                <ul className={" text-md pt-1 text-blue-500 " + (displaydropdown ? "" : "hidden")}>
-                                    <li className="">
-                                        <Link
-                                            className="rounded-t  py-2 px-4 block whitespace-no-wrap"
-                                            to="/category/electronics"
-                                        >Electronics
-                                        </Link>
-                                    </li>
-                                    <li className="">
-                                        <Link
-                                            className=" py-2  px-3 block whitespace-no-wrap"
-                                            to="/category/jewelery"
-                                        >Jewelery
-                                        </Link>
-                                    </li>
-                                    
-                                </ul>
                             </div>
-                            <div className="p-4">
-                                <Link to="/cart"><div className="flex items-center"><RiShoppingCartLine className="text-2xl" /><span className="pl-2">Cart</span></div></Link>
-                            </div>
-                        </ul>
-                    </div>
+                            <ul className="mt-12 text-white">
+                                <div className="p-4">
+                                    <Link to="/">
+                                        <div className="flex items-center">
+                                            <HiHome />
+                                            <span className="pl-2">
+                                                Home
+                                            </span>
+                                        </div>
+                                    </Link>
+                                </div>
+                                <div className="p-4">
+
+                                    <div className="flex items-center" onClick={() => setdisplaydropdown(!displaydropdown)}>
+                                        <span className="pl-2">
+                                            Category
+
+                                        </span>
+                                        <RiArrowDropDownLine className="text-4xl pt-1" />
+                                    </div>
+                                    <ul className={" text-md pt-1 text-blue-500 " + (displaydropdown ? "" : "hidden")}>
+                                        <li className="">
+                                            <Link
+                                                className="rounded-t  py-2 px-4 block whitespace-no-wrap"
+                                                to="/category/electronics"
+                                            >Electronics
+                                            </Link>
+                                        </li>
+                                        <li className="">
+                                            <Link
+                                                className=" py-2  px-3 block whitespace-no-wrap"
+                                                to="/category/jewelery"
+                                            >Jewelery
+                                            </Link>
+                                        </li>
+
+                                    </ul>
+                                </div>
+                                <div className="p-4">
+                                    <Link to="/cart"><div className="flex items-center"><RiShoppingCartLine className="text-2xl" /><span className="pl-2">Cart</span></div></Link>
+                                </div>
+                            </ul>
+                        </div>
+                    </motion.div>
                 </div>
-            </div>
+            }
 
         </div>
     )
