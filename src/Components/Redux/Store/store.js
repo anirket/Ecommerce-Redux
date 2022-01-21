aef195b1c4b9ed88b458a126badf31e19e302434
Aniket Kulkarni <anirajkul01@gmail.com>
2022-01-22 02:23:08 +0530

    added redux persist

--- src/Components/Redux/Store/store.js
+++ src/Components/Redux/Store/store.js
@@ -2,10 +2,23 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import { rootReducer } from '../Reducers/index'
 import logger from 'redux-logger'
 import ReduxThunk from 'redux-thunk'
+import { persistStore, persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
+
+//localstorage saved name
+const persistConfig = {
+    key: 'root',
+    storage
+}
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
-export const store = createStore(rootReducer, /* preloadedState, */ composeEnhancers(
+
+const store = createStore(persistedReducer, /* preloadedState, */ composeEnhancers(
     applyMiddleware(logger, ReduxThunk)
 ));
+let persistor = persistStore(store)
+
+export { store, persistor }
--- src/index.js
+++ src/index.js
@@ -2,25 +2,25 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App';
 import './index.css'
-import { store } from './Components/Redux/Store/store'
+import { store, persistor } from './Components/Redux/Store/store'
 import { Provider } from 'react-redux'
 import { transitions, positions, Provider as AlertProvider } from 'react-alert'
 import AlertTemplate from 'react-alert-template-basic'
-
+import { PersistGate } from 'redux-persist/integration/react'
 
 const options = {
-    // you can also just use 'bottom center'
     position: positions.BOTTOM_CENTER,
     timeout: 5000,
     offset: '30px',
-    // you can also just use 'scale'
     transition: transitions.SCALE
 }
 
 ReactDOM.render(
     <Provider store={store}>
-        <AlertProvider template={AlertTemplate} {...options}>
-            <App />
-        </AlertProvider>
+        <PersistGate loading={null} persistor={persistor}>
+            <AlertProvider template={AlertTemplate} {...options}>
+                <App />
+            </AlertProvider>
+        </PersistGate>
     </Provider>
     , document.getElementById("root"));
